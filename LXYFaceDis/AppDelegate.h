3139f271fce968b1662819d996c1ca406d54f1f3
liuxy <>
2018-08-17 15:12:21 +0800

    创建文件

---
+++ LXYFaceDis/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LXYFaceDis
+//
+//  Created by liuxy on 2018/8/17.
+//  Copyright © 2018年 liuxy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LXYFaceDis/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LXYFaceDis
+//
+//  Created by liuxy on 2018/8/17.
+//  Copyright © 2018年 liuxy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
